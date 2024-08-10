419fa40b2d4a4f344be32ad67f62ca1a2ed97f63
Mainuddin Rizvi <web97development@gmail.com>
2024-08-11 04:27:34 +0530

    Add Hero Heading and subtext in section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative w-full h-screen mx-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row item-start gap-5`}
+      >
+        <div className="flex flex-col justify-center mt-5">
+          <div className="rounded-full bg-[#915efff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white-100`}>
+            Hi, I'm <span className="text-[#915eff]">Mainuddin</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I developed 3D visuals, user <br className="sm:block hidden" />
+            interfaces and web applications
+          </p>
+        </div>
+      </div>
+      <ComputersCanvas />
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
