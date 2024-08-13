2d1bb0633bc186499fc0f3a97843eecab61c12e8
Mainuddin Rizvi <web97development@gmail.com>
2024-08-14 00:58:27 +0530

    Add Contact Section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,16 +33,16 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_mc996py",
+        "template_41g03f8",
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Mainuddin Rizvi",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "mainu97rizvi@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "RR0UiYzVNskkSXwFx"
       )
       .then(
         () => {
